1b09d30ec49816fd48c922654058043f00a93e11
yousef <yousef.shaheen777@gmail.com>
2019-10-04 11:42:45 -0500

    HouseList is now modular == HouseSummary is implemented

--- spike/src/components/houses/HouseList.js
+++ spike/src/components/houses/HouseList.js
@@ -1,32 +1,13 @@
 import React from 'react'
+import HouseSummary from './HouseSummary'
 
 const HouseList = () => {
   return (
     <div className="house-list section">
-
-      <div className="card z-depth-0 house-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">House Title</span>
-          <p>Poseted by "USER"</p>
-          <p className="grey-text">The Date 01/1/1991</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 house-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">House Title</span>
-          <p>Poseted by "USER"</p>
-          <p className="grey-text">The Date 01/1/1991</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 house-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">House Title</span>
-          <p>Poseted by "USER"</p>
-          <p className="grey-text">The Date 01/1/1991</p>
-        </div>
-      </div>
+      <HouseSummary />
+      <HouseSummary />
+      <HouseSummary />
+    
     </div>
   )
 }
---
+++ spike/src/components/houses/HouseSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HouseSummary = () => {
+  return(
+    <div className="card z-depth-0 house-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">House Title</span>
+        <p>Poseted by "USER"</p>
+        <p className="grey-text">The Date 01/1/1991</p>
+      </div>
+    </div>
+  )
+}
+
+export default HouseSummary
