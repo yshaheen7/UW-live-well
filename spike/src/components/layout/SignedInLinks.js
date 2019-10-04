d841f672fcfcde07f3d059e02d54d7326ccc5156
yousef <yousef.shaheen777@gmail.com>
2019-10-04 16:10:47 -0500

    SignedIn / SingedOut links now works

--- spike/src/components/layout/SignedInLinks.js
+++ spike/src/components/layout/SignedInLinks.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const SignedInLinks = () => {
   return (
     <ul className="right">
-      <li><NavLink to='/'>Add a House</NavLink></li>
+      <li><NavLink to='/addhouse'>Add a House</NavLink></li>
       <li><NavLink to='/'>Log out</NavLink></li>
       <li><NavLink to='/' className='btn btn-floating pink lighten-1'>NN</NavLink></li>
     </ul>
